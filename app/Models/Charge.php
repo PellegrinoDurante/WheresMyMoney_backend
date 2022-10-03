efcf6b5d53bbf90a2adb67e394a434b9a56de30f
Pellegrino Durante <pellegrino.durante@gmail.com>
2022-10-04 00:58:37 +0200

    fix: restore amount column to integer

--- app/Models/Charge.php
+++ app/Models/Charge.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Barryvdh\LaravelIdeHelper\Eloquent;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -51,5 +52,13 @@ class Charge extends Model
     public function recurringExpense(): BelongsTo
     {
         return $this->belongsTo(RecurringExpense::class);
+    }
+
+    protected function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
     }
 }
