5f63aa3c54b23ef2b3f0fae692be707b064c20ab
Pellegrino Durante <pellegrino.durante@gmail.com>
2023-02-28 22:05:56 +0100

    chore: refactor transaction's page actions

--- app/Filament/Resources/TransactionResource/Pages/ManageTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ManageTransactions.php
@@ -21,24 +21,25 @@ class ManageTransactions extends ManageRecords
     protected function getActions(): array
     {
         $bankService = app(NordigenService::class);
-        return [
-            Actions\Action::make('sync')
-                ->label(__('transactions.sync'))
-                ->action(function ($data) use ($bankService) {
-                    $bankService->syncTransactions(AccessToken::findOrFail($data['bank_account']));
-                })
-                ->form([
-                    Select::make('bank_account')
-                        ->label(__('transactions.bank_account'))
-                        ->options(
-                            AccessToken::where('type', '=', AccessToken::TYPE_BANK)
-                                ->where('provider', '=', AccessToken::PROVIDER_BANK)
-                                ->get()
-                                ->pluck('name', 'id')
-                        )
-                ]),
-            Actions\CreateAction::make(),
-        ];
+
+        $actions = parent::getActions();
+        $actions[] = Actions\Action::make('sync')
+            ->label(__('transactions.sync'))
+            ->action(function ($data) use ($bankService) {
+                $bankService->syncTransactions(AccessToken::findOrFail($data['bank_account']));
+            })
+            ->form([
+                Select::make('bank_account')
+                    ->label(__('transactions.bank_account'))
+                    ->options(
+                        AccessToken::where('type', '=', AccessToken::TYPE_BANK)
+                            ->where('provider', '=', AccessToken::PROVIDER_BANK)
+                            ->get()
+                            ->pluck('name', 'id')
+                    )
+            ]);
+
+        return $actions;
     }
 
     protected function getHeaderWidgets(): array
